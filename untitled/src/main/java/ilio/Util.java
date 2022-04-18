a7b80d508f05c2d24b1ab8c4049fb760b96da112
qingyang.zhong <qizhong@coupang.com>
2022-04-18 16:46:34 +0800

    0000

---
+++ untitled/src/main/java/ilio/Util.java
@@ -0,0 +1,12 @@
+package ilio;
+
+public class Util {
+
+    public static int sum2(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int sum3(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+}
---
+++ untitled/src/test/java/ilio/UtilTest.java
@@ -0,0 +1,14 @@
+package ilio;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilTest {
+
+    @Test
+    public void test_sum2() {
+        assertEquals(Util.sum2(1, 2), 3);
+    }
+
+}
